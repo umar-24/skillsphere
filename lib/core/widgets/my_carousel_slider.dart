ecfa9d9aed526a11d0d83812fd863c3f52101950
Umar <umarshyk683@gmail.com>
2024-11-19 19:58:57 +0500

    Add slider

--- lib/core/widgets/my_carousel_slider.dart
+++ lib/core/widgets/my_carousel_slider.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:skillsphere/core/constants/colors.dart';
